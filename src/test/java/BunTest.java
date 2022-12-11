5c8dbcfefce597a26c09e206bedf5f0da6abdda0
Nikita Poleschuk <poleshchuk.nikita@yandex.ru>
2022-12-11 18:18:44 +0300

    second commit made java 11 made BunTest and IngredientTest parameterized

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -1,15 +1,85 @@
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import praktikum.Bun;
 
+@RunWith(Parameterized.class)
 public class BunTest {
 
-    @Test
-    public void BunIsCreated(){
-        Bun bun = new Bun("purple",123);
-        float price = 123.0f;
+    private final String colour;
+    private final float price;
+    private final float expectedPrice;
 
-        Assert.assertEquals("название булки не соответствует ожиданию","purple",bun.getName());
-        Assert.assertEquals("цена булки не соответствует ожиданию",price,bun.getPrice(),0);
+    public BunTest(String colour, float price, float expectedPrice) {
+        this.colour = colour;
+        this.price = price;
+        this.expectedPrice = expectedPrice;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getBunsVariations() {
+        //happy path
+        String name1="purple";
+        float price1=123;
+        float expectedPrice1=123.0f;
+        //price with floating point
+        String name2="purple";
+        float price2=123.0f;
+        float expectedPrice2=123.0f;
+        //empty colour
+        String name3="";
+        float price3=123;
+        float expectedPrice3=123.0f;
+        //null colour
+        String name4=null;
+        float price4=123;
+        float expectedPrice4=123.0f;
+        //special characters colour
+        String name5="!";
+        float price5=123;
+        float expectedPrice5=123.0f;
+        //upper case colour
+        String name6="PURPLE";
+        float price6=123;
+        float expectedPrice6=123.0f;
+        //zero price
+        String name7="purple";
+        float price7=0;
+        float expectedPrice7=0.0f;
+        //under zero price
+        String name8="purple";
+        float price8=-1;
+        float expectedPrice8=-1.0f;
+        //under zero price
+        String name9="purple";
+        float price9=Float.MAX_VALUE;
+        float expectedPrice9=Float.MAX_VALUE;
+        //min price with floating pount
+        String name10="purple";
+        float price10=0.01f;
+        float expectedPrice10=0.01f;
+
+
+        return new Object[][]{
+                {name1, price1, expectedPrice1},
+                {name2, price2, expectedPrice2},
+                {name3, price3, expectedPrice3},
+                {name4, price4, expectedPrice4},
+                {name5, price5, expectedPrice5},
+                {name6, price6, expectedPrice6},
+                {name7, price7, expectedPrice7},
+                {name8, price8, expectedPrice8},
+                {name9, price9, expectedPrice9},
+                {name10, price10, expectedPrice10},
+        };
+    }
+
+    @Test
+    public void BunIsCreated() {
+        Bun bun = new Bun(colour, price);
+
+        Assert.assertEquals("название булки не соответствует ожиданию", colour, bun.getName());
+        Assert.assertEquals("цена булки не соответствует ожиданию", expectedPrice, bun.getPrice(), 0);
     }
 }
--- src/test/java/IngredientTest.java
+++ src/test/java/IngredientTest.java
@@ -1,18 +1,34 @@
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import praktikum.Ingredient;
 import praktikum.IngredientType;
 
+@RunWith(Parameterized.class)
 public class IngredientTest {
+    private final IngredientType ingredientType;
 
+    public IngredientTest(IngredientType ingredientType) {
+        this.ingredientType = ingredientType;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getIngredientTypes() {
+
+
+        return new Object[][]{
+                {IngredientType.SAUCE},
+                {IngredientType.FILLING},
+        };
+    }
 
     @Test
-    public void checkIngredientIsCreated() {
-        Ingredient ingredient = new Ingredient(IngredientType.SAUCE, "emptiness", 123);
+    public void checkIngredientIsCreated() throws IllegalArgumentException {
+        Ingredient ingredient = new Ingredient(ingredientType, "emptiness", 123);
         float price = 123.0f;
-
-        Assert.assertEquals("тип интредиента не соответствует ожидаемому", "SAUCE", ingredient.getType().toString());
+        Assert.assertEquals("тип интредиента не соответствует ожидаемому", ingredientType.name(), ingredient.getType().toString());
         Assert.assertEquals("название интредиента не соответствует ожидаемому", "emptiness", ingredient.getName());
-        Assert.assertEquals("цена булки не соответствует ожиданию", price, ingredient.getPrice(),0);
+        Assert.assertEquals("цена булки не соответствует ожиданию", price, ingredient.getPrice(), 0);
     }
 }
